34c24d0518e8aee0a892779ee21f5070ddecd755
pazhanivel28 <pazhanivel2806@gmail.com>
2022-11-23 15:04:34 +0530

    added serial number

--- src/container/DataView.js
+++ src/container/DataView.js
@@ -5,9 +5,10 @@ function DataView() {
   const userData = useSelector((state) => state.addData.data);
   return (
     <div>
-      {userData.map((value) => {
+      {userData.map((value,index) => {
         return (
           <div key={value.email}>
+           <div>{index+1}</div>
             <div>{value.name}</div>
             <div>{value.email}</div>
             <div>{value.age}</div>
